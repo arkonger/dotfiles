63934a130b79b16a64ab3fe2c0b7b974578fcf72
Ash Konger <arkonger@gmail.com>
2024-06-09 19:28:29 -0400

    Added markdown-preview plugin

--- .config/nvim/lazy.lua
+++ .config/nvim/lazy.lua
@@ -137,4 +137,10 @@ require("lazy").setup({
       })
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 })
